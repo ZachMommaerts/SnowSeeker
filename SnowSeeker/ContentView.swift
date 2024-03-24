3253b4824e5f06973d711efdc103a7af25e0fb84
ZachMommaerts <zmommaerts@gmail.com>
2024-03-24 18:34:52 -0500

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Zach Mommaerts on 3/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Zach Mommaerts on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
